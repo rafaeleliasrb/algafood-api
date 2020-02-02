2f6551e3af900262a865b539dc0e369efd19b3d4
Rafael Elias <13rafael.elias@gmail.com>
2020-02-02 20:54:39 -0200

    Removendo codigo comentado

--- src/main/java/com/algaworks/algafoodapi/domain/service/PedidoService.java
+++ src/main/java/com/algaworks/algafoodapi/domain/service/PedidoService.java
@@ -11,22 +11,10 @@ import com.algaworks.algafoodapi.domain.repository.PedidoRepository;
 public class PedidoService {
 
 	private final PedidoRepository pedidoRepository;
-	/*private final ProdutoService produtoService;
-	private final RestauranteService restauranteService;
-	private final FormaPagamentoService formaPagamentoService;
-	private final UsuarioService usuarioService;
-	private final CidadeService cidadeService;*/
 
 	@Autowired
-	public PedidoService(PedidoRepository pedidoRepository/*, ProdutoService produtoService,
-			RestauranteService restauranteService, FormaPagamentoService formaPagamentoService,
-			UsuarioService usuarioService, CidadeService cidadeService*/) {
+	public PedidoService(PedidoRepository pedidoRepository) {
 		this.pedidoRepository = pedidoRepository;
-		/*this.produtoService = produtoService;
-		this.restauranteService = restauranteService;
-		this.formaPagamentoService = formaPagamentoService;
-		this.usuarioService = usuarioService;
-		this.cidadeService = cidadeService;*/
 	}
 	
 	public Pedido buscarOuFalhar(String codigoPedido) {
@@ -36,35 +24,6 @@ public class PedidoService {
 
 	@Transactional
 	public Pedido adicionarPedido(Pedido pedido) {
-			/*validarPedido(pedido);
-			validarItensPedido(pedido);
-	
-			pedido.setTaxaFrete(pedido.getRestaurante().getTaxaFrete());
-			pedido.calcularValorTotal(); */
-			
-			return pedidoRepository.save(pedido);
+		return pedidoRepository.save(pedido);
 	}
-
-	/* private void validarItensPedido(Pedido pedido) {
-		pedido.getItens().forEach(item -> {
-			Produto produto = produtoService.buscarOuFalhar(item.getProduto().getId(), pedido.getRestaurante());
-			item.setPedido(pedido);
-			item.setProduto(produto);
-			item.setPrecoUnitario(produto.getPreco());
-		});
-	}
-
-	private void validarPedido(Pedido pedido) {
-		Restaurante restaurante = restauranteService.buscarOuFalha(pedido.getRestaurante().getId());
-		FormaPagamento formaPagamento = formaPagamentoService
-				.buscarOuFalharDeUmRestaurante(pedido.getFormaPagamento().getId(), restaurante);
-		//TODO temporariamente setando cliente 1
-		Usuario cliente = usuarioService.buscarOuFalhar(1L);
-		Cidade cidade = cidadeService.buscarOuFalhar(pedido.getEnderecoEntrega().getCidade().getId()); 
-
-		pedido.setRestaurante(restaurante);
-		pedido.setFormaPagamento(formaPagamento);
-		pedido.setCliente(cliente);
-		pedido.getEnderecoEntrega().setCidade(cidade);
-	} */
 }
